f737fac78f24f2cab44bc9a2d54d0ca8f83c186f
Karan Uike <karanuike666@gmail.com>
2026-07-07 09:04:06 +0530

    Inheritance

--- oops_p.java
+++ oops_p.java
@@ -2,22 +2,49 @@ package DSA.OOPS;
 
 public class oops_p {
     public static void main (String args[]){
-        Student s1 = new Student("Karan",149);
-        System.out.println(s1.name);
-        System.out.println(s1.rollno);
+        fish shark = new fish();
+        shark.eat();
+        shark.breath();
+        // Student s1 = new Student("Karan",149);
+        // System.out.println(s1.name);
+        // System.out.println(s1.rollno);
     
     }
 }
 
-class Student{
-    String name;
-    int rollno;
+// class Student{
+//     String name;
+//     int rollno;
 
-    Student(String name , int rollno){
-        this.name = name;
-        this.rollno = rollno;
+//     Student(String name , int rollno){
+//         this.name = name;
+//         this.rollno = rollno;
         
-        //System.out.println();
+//         //System.out.println();
+//     }
+
+// }
+
+//Inheritane
+//gparents class ->parent class -> child class
+
+class animal{//parent class
+    String color;
+    void eat(){
+        System.out.println("eating");
+    }
+    void breath(){
+        System.out.println("breathing");
+    }
+}
+
+//child class
+class fish extends animal{
+    int fins;
+
+    void swims(){
+        System.out.println("swimming under the water!");
+    
     }
 
 }
